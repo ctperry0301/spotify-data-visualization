f217d92fcb0e47203a78780408fe75e0ad776457
Jack Fales <46923410+jackfales@users.noreply.github.com>
2021-04-13 16:31:46 -0700

    Fixed backend bugs

--- spotify-data-visualization/server.js
+++ spotify-data-visualization/server.js
@@ -1,23 +1,23 @@
 var request = require('request');
 var credentials = require('./client-credentials.json');
 
-var authOptions = {
-   url: 'https://accounts.spotify.com/api/token',
-   headers: {
-     'Authorization': 'Basic ' + (new Buffer(credentials.client_id + ':' + credentials.client_secret).toString('base64'))
-   },
-   form: {
-     grant_type: 'client_credentials'
-   },
-   json: true
-};
-
 /**
  * Authenticates a user based on credentials given in client-credentials.json
  * 
  * Returns the access token
  */
-export const login = () => {
+const login = () => {
+   var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        'Authorization': 'Basic ' + (credentials.client_id + ':' + credentials.client_secret).toString('base64')
+      },
+      form: {
+        grant_type: 'client_credentials'
+      },
+      json: true
+   };
+
    request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200)
          return body.access_token;
@@ -29,7 +29,7 @@ export const login = () => {
  * 
  * @param {*} token: access token 
  */
-export const getJmPerez = (token) => {
+const getJmPerez = (token) => {
    var options = {
       url: 'https://api.spotify.com/v1/users/jmperezperez',
       headers: {
@@ -40,6 +40,25 @@ export const getJmPerez = (token) => {
    request.get(options, function(error, response, body) {
       console.log(body);
    });
-}
+};
 
+/**
+ * Retrieves n featured playlists and logs it to the console
+ * 
+ * @param {*} token: access token 
+ * @param {Number} n: number of playlists to be retrieved (1-50 inclusive)
+ */
+const getFeaturedPlaylists = (token, n) => {
+   var options = {
+      url: 'https://api.spotify.com/v1/browse/featured-playlists',
+      headers: {
+      'Authorization': 'Bearer ' + token
+      },
+      json: true
+   };
+   request.get(options, function(error, response, body) {
+      console.log(body);
+   });
+};
 
+module.exports = { login, getJmPerez, getFeaturedPlaylists };
